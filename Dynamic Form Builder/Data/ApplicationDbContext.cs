dae0af2c8d92fde2063022d04a465b625edd1f8a
Abrar Hanif Riad <ahriad.cse@gmail.com>
2025-07-23 13:49:09 +0600

    ini

---
+++ Dynamic Form Builder/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dynamic_Form_Builder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
